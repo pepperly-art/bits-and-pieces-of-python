296eefe037140dbd9b78515c0aee47c4bebb9ee7
Pepperly <pepperly@live.com>
2026-02-26 23:38:57 -0500

    30 Days of Python Day 03 (needs fixes)

--- 30DaysOfPython/Day_Two_course.py
+++ 30DaysOfPython/Day_Two_course.py
@@ -77,8 +77,8 @@
 # num_int = int(num_float)
 # print('num_int', int(num_int))      # 10
 
-# str to list
-first_name = 'Asabeneh'
-print(first_name)               # 'Asabeneh'
-first_name_to_list = list(first_name)
-print(first_name_to_list)            # ['A', 's', 'a', 'b', 'e', 'n', 'e', 'h']
+# # str to list
+# first_name = 'Asabeneh'
+# print(first_name)               # 'Asabeneh'
+# first_name_to_list = list(first_name)
+# print(first_name_to_list)            # ['A', 's', 'a', 'b', 'e', 'n', 'e', 'h']
--- README.md
+++ README.md
@@ -29,3 +29,11 @@ else:
 is equal to
 
 return not x
+
+
+In addition to the above comparison operator Python uses:
+
+- is: Returns true if both variables are the same object(x is y)
+- is not: Returns true if both variables are not the same object(x is not y)
+- in: Returns True if the queried list contains a certain item(x in y)
+- not in: Returns True if the queried list doesn't have a certain item(x not in y)
